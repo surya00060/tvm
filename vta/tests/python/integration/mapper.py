b89cae922d1db936c866353431f3182a925797a7
Anand <anandug25999@gmail.com>
2020-05-18 11:18:31 +0530

    Elucidated pseudocode

--- vta/tests/python/integration/mapper.py
+++ vta/tests/python/integration/mapper.py
@@ -13,6 +13,8 @@ def systolic_fold_conv(ConvParams,TileParams,tile_pos,):
     F1,F2   = TileParams[3]
 
     n,c,m,e,f = tile_pos
+
+
     
     #Things to do:
     '''
@@ -31,10 +33,36 @@ def systolic_fold_conv(ConvParams,TileParams,tile_pos,):
     '''
     Input Channels map to rows of systolic
     Output Channels map to columns of systolic
-    Weight stationary Implementation, fixes ordering to some e
+    Weight stationary Implementation, fixes ordering for some loops
     '''
-    
 
+    #The next 2 loops are interchangable
+    for r in range(R):
+        for s in range(S):
+            # Load Weights Here, using the following logic :
+            # column j of systolic has the jth output filter
+            # row i of the systolic will have the weights corresponding to the ith input channel
+            # so in the element i,j, you have Weight(j , i, r , s) (in MCRS fashion, (Not sure if this is the convention we have to follow))
+
+
+            #Now we have to deal with the inputs
+
+            # We have the following variables here : i,j, time
+            # Calculate the area of effect of the filter ^ Output tile ^ channel : Let this tile be of size H_eff*W_eff
+            # In this tile, We have, input at clock cycle t , in the ith row should be row major flattened input
+            # of all pixels with (x > Sx*r, y >  Sy*s), With offset of 1/column as the columns go along
+
+
+            #Now, Where to store the outputs:
+            
+            # If you have chosen row major previously, your outputs will also be in row major order, U will have to accumulate them 
+            # in a buffer
+
+
+
+
+            dummy =0 # Code to avoid syntax error
+    
 
     return ""
 
@@ -52,14 +80,18 @@ def mapper(ConvParams, TileParams,op):
 
     lower = ""
     #TODO Check illegal configurations
+
     #Lets just see 1 tile first :
     for n in range(N):
-        for c in range(C1):
-            for m in range(M1):
-                for e in range(E1):
-                    for f in range(F1):
+        for m in range(M1):
+            for e in range(E1):
+                for f in range(F1):
+                    #Output = 0 here
+                    for c in range(C1):
                         tile_pos = [n,c,m,e,f]
                         lower += systolic_fold_conv(ConvParams,TileParams,tile_pos)
+
+                    #Flush outputs here
 
 
 
